9f92c6140fcb6bf6ab75b256ed3d1e6f02bdf72c
kohei-iwase <me.tekelily@gmail.com>
2020-03-02 07:24:17 +0000

    タイトルカラム追加、マイグレーション実施

--- db/migrate/20170307140030_create_books.rb
+++ db/migrate/20170307140030_create_books.rb
@@ -1,7 +1,8 @@
 class CreateBooks < ActiveRecord::Migration[5.0]
   def change
     create_table :books do |t|
-      t.text :body
+      t.string 	:title
+      t.text 	:body
 
       t.timestamps
     end
